fbb44494558487015f99c723180f9563f1a745c7
aokeson <Alexandra.Okeson@colorado.edu>
2018-02-21 16:22:49 -0800

    try dividing grad and hess by label

--- src/objective/regression_obj.cc
+++ src/objective/regression_obj.cc
@@ -274,8 +274,8 @@ class CoxRegression : public ObjFunction {
 
       //const bst_float grad = exp_p*r_k - static_cast<bst_float>(y > 0);
       //const bst_float hess = exp_p*r_k - exp_p*exp_p * s_k;
-      const double grad = exp_p*r_k - static_cast<double>(y > 0);
-      const double hess = exp_p*r_k - exp_p*exp_p * s_k;
+      const double grad = (exp_p*r_k - static_cast<double>(y > 0))/std::abs(y);
+      const double hess = (exp_p*r_k - exp_p*exp_p * s_k)/std::abs(y);
 
       if (std::abs(y) >= last_abs_y) {
         out_gpair->at(i) = bst_gpair(grad * w, hess * w);
@@ -363,11 +363,11 @@ class CoxBreslowRegression : public ObjFunction {
         s_k += 1.0/(exp_p_sum*exp_p_sum);
       }
       
-      const double grad = exp_p*r_k - static_cast<double>(y > 0);
+      const double grad = (exp_p*r_k - static_cast<double>(y > 0))/std::abs(y);
       const double hess = exp_p*r_k - exp_p*exp_p * s_k;
 
       if (std::abs(y) >= last_abs_y) {
-        out_gpair->at(i) = bst_gpair(grad * w, hess * w);
+        out_gpair->at(i) = (bst_gpair(grad * w, hess * w))/std::abs(y);
       } else {
         label_correct = false;
         break;
@@ -478,8 +478,8 @@ class CoxEfronRegression : public ObjFunction {
         s_k += 1.0/(denom*denom);
       }
       
-      const double grad = (1-coeff)*exp_p*r_k - static_cast<double>(y > 0);
-      const double hess = (1-coeff)*exp_p*r_k - (1-coeff)*(1-coeff)*exp_p*exp_p * s_k;
+      const double grad = ((1-coeff)*exp_p*r_k - static_cast<double>(y > 0))/std::abs(y);
+      const double hess = ((1-coeff)*exp_p*r_k - (1-coeff)*(1-coeff)*exp_p*exp_p * s_k)/std::abs(y);
 
       if (std::abs(y) >= last_abs_y) {
         out_gpair->at(i) = bst_gpair(grad * w, hess * w);
